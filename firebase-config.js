06298304e97df87168bbb274edc6a5fdad9f9f28
Pacocone <pacoconesa@vaquelia.com>
2025-08-03 12:10:30 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Pega aquí tu configuración web de Firebase
+
 window.FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
-  measurementId: "OPCIONAL"
+  apiKey: "…",
+  authDomain: "restaurantworld-86f53.firebaseapp.com",
+  projectId: "restaurantworld-86f53",
+  storageBucket: "restaurantworld-86f53.firebasestorage.app",
+  messagingSenderId: "459185197746",
+  appId: "1:459185197746:web:327c97c4a64f1757b56fab",
+  measurementId: "G-LKKH1T8ZP7"
 };
