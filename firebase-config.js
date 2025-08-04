640ca2bda10ac097d5e4be76ed2db2e41fc6f529
Pacocone <pacoconesa@vaquelia.com>
2025-08-04 12:27:06 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
-window.FIREBASE_CONFIG = {
+// Replace with your Firebase config
+const firebaseConfig = {
   apiKey: "AIzaSyDULX4PkHaJR35pFtxcNlUKzXJ3lMiJBjM",
   authDomain: "restaurantworld-86f53.firebaseapp.com",
   projectId: "restaurantworld-86f53",
   storageBucket: "restaurantworld-86f53.firebasestorage.app",
-  messagingSenderId: "459185197746",
+  messagingSenderId:  "459185197746",
   appId: "1:459185197746:web:327c97c4a64f1757b56fab",
-  measurementId: "G-LKKH1T8ZP7"
 };
-
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.firestore();
+window.auth = firebase.auth();
